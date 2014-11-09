91f084af885ca9f1f3ca19a36612d94bc0b4133b
LeeChinKong <lee.c.kong@gmail.com>
2014-11-09 22:15:35 +1100

    Added RobotController class
    implement processCommand

--- lib/toy_robot/robot_controller.rb
+++ lib/toy_robot/robot_controller.rb
@@ -2,4 +2,12 @@
 # To process command, control robot movement and display result.
 
 class RobotController
+
+  # Process command
+  def processCommand(command)
+    if command =~ /^PLACE\s\d+,\d+,NORTH|EAST|SOUTH|WEST$/
+      return true
+    end
+    return false
+  end
 end
