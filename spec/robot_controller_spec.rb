dd67305447322d8c5644f88abbd33978e2608d07
LeeChinKong <lee.c.kong@gmail.com>
2014-11-09 22:56:41 +1100

    Update RobotController.processCommand test
    * subsequent commands test
    * invalid x and y test

--- spec/robot_controller_spec.rb
+++ spec/robot_controller_spec.rb
@@ -23,7 +23,7 @@ describe RobotController do
     it "when command is unknown" do
       expect(@controller.processCommand("UNKNOWN")).to eq(false)
     end
-    it "when command is an not the expected first command" do
+    it "when command is not the expected first command" do
       expect(@controller.processCommand("MOVE")).to eq(false)
       expect(@controller.processCommand("LEFT")).to eq(false)
       expect(@controller.processCommand("RIGHT")).to eq(false)
@@ -31,10 +31,42 @@ describe RobotController do
     it "when command is an invalid first command" do
       expect(@controller.processCommand("PLACE")).to eq(false)
       expect(@controller.processCommand("PLACE 0,0")).to eq(false)
-      expect(@controller.processCommand("PLACE NORTH")).to eq(false)           
+      expect(@controller.processCommand("PLACE NORTH")).to eq(false)  
+      expect(@controller.processCommand("PLACE 0,0,UNKNOWN")).to eq(false)         
     end
-    it "when command is valid first command" do
+    it "when command is a valid first command" do
       expect(@controller.processCommand("PLACE 0,0,NORTH")).to eq(true)
+    end
+    it "when command is an invalid subsequent command" do
+      # Place the robot on the table first
+      @controller.processCommand("PLACE 0,0,NORTH")
+      # Test if subsequent invalid commands failed
+      expect(@controller.processCommand(nil)).to eq(false)
+      expect(@controller.processCommand("UNKNOWN")).to eq(false)
+    end
+    it "when command is a valid subsequent command" do
+      # Place the robot on the table first
+      @controller.processCommand("PLACE 0,0,NORTH")
+      # Test if subsequent valid commands are successful
+      expect(@controller.processCommand("PLACE 0,0,NORTH")).to eq(true)
+      expect(@controller.processCommand("MOVE")).to eq(true)
+      expect(@controller.processCommand("LEFT")).to eq(true)
+      expect(@controller.processCommand("RIGHT")).to eq(true)
+    end
+    it "when command is mixed or lowercases" do
+      # Place the robot on the table first
+      @controller.processCommand("PLACE 0,0,NORTH")
+      # Test if subsequent valid commands are successful
+      expect(@controller.processCommand("place 0,0,North")).to eq(true)
+      expect(@controller.processCommand("mOvE")).to eq(true)
+      expect(@controller.processCommand("left")).to eq(true)
+      expect(@controller.processCommand("RIGHT")).to eq(true)
+    end
+    it "when PLACE command has invalid x and y" do
+      # Test when x and y is greater than the table dimension
+      expect(@controller.processCommand("PLACE 5,5,NORTH")).to eq(false) 
+      # Test when x and y is nagative value     
+      expect(@controller.processCommand("PLACE -1,-1,NORTH")).to eq(false)     
     end
   end
   
