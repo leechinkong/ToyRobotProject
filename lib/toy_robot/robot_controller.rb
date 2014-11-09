5cd9b355f0f1b95a5b4258f3277506fb097d989f
LeeChinKong <lee.c.kong@gmail.com>
2014-11-09 23:23:02 +1100

    Update RobotController.processCommand code
    * convert command to upper case
    * validate subsequent commands
    * validate x and y

--- lib/toy_robot/robot_controller.rb
+++ lib/toy_robot/robot_controller.rb
@@ -3,11 +3,45 @@
 
 class RobotController
 
-  # Process command
+  # Default table width and height (in units)
+  WIDTH = 5
+  HEIGHT = 5
+
+  # Process command.
+  # = Parameters
+  # - +command+:: the command to control robot
+  # = Returns
+  # - true when command is processed successfully
   def processCommand(command)
-    if command =~ /^PLACE\s\d+,\d+,NORTH|EAST|SOUTH|WEST$/
-      return true
+    # Check if the command is nil
+    if command != nil
+      # convert command to all uppercase
+      command = command.upcase
+
+      # Check if command is a valid PLACE
+      if command =~ /^PLACE\s\d+,\d+,NORTH|EAST|SOUTH|WEST$/
+        # Get x and y 
+        placement = command.scan(/\d+/)
+        x = placement[0].to_i
+        y = placement[1].to_i
+
+        # Validate x and y, make sure x and y is within range
+        if x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT
+          # Remember that the robot is placed
+          @placed = true
+          return true
+        end
+      # Check if the robot is placed
+      elsif @placed == true
+        # Check if command is a valid subsequent command
+        if command =~ /^MOVE|LEFT|RIGHT$/
+          return true
+        end
+      end
     end
+
+    # Invalid command
     return false
   end
+
 end
