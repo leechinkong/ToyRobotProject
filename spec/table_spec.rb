1505fd4446ea13e8605902f68ae817313f7f5339
LeeChinKong <lee.c.kong@gmail.com>
2014-11-10 14:02:09 +1100

    Added table width and height validation tests
    expect ArgumentError when table width and height is not configured
    correctly (must be numeric and greater than 0)

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -16,8 +16,14 @@ describe Table do
       expect(table.width).to eq(10)
       expect(table.height).to eq(8)
     end
+    it "when width and height is not numeric" do
+      expect(Table.new("","")).to raise_error(ArgumentError)
+    end
+    it "when width and height is less than 1" do
+      expect(Table.new(0,0)).to raise_error(ArgumentError)
+      expect(Table.new(-5,-5)).to raise_error(ArgumentError)
+    end
   end
-
   
    # Test validateXY
   describe "#validate" do
