facf416320013e5206aa24a023de23cc15fb4d7b
LeeChinKong <lee.c.kong@gmail.com>
2014-11-10 14:28:36 +1100

    Added robot x,y validation tests
    x and y must be numeric

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -6,17 +6,22 @@ describe Robot do
   # Test initialize
   describe "#new" do
     it "is an instance of Robot" do
-      @robot = Robot.new(0,0,Direction::N)
-      expect(@robot).to be_an_instance_of(Robot)
-      expect(@robot.x).to eq(0)
-      expect(@robot.y).to eq(0)
-      expect(@robot.face).to eq(Direction::N)
+      robot = Robot.new(0,0,Direction::N)
+      expect(robot).to be_an_instance_of(Robot)
+      expect(robot.x).to eq(0)
+      expect(robot.y).to eq(0)
+      expect(robot.face).to eq(Direction::N)
 
-      @robot = Robot.new(2,3,Direction::E)
-      expect(@robot).to be_an_instance_of(Robot)
-      expect(@robot.x).to eq(2)
-      expect(@robot.y).to eq(3)
-      expect(@robot.face).to eq(Direction::E)
+      robot = Robot.new(2,3,Direction::E)
+      expect(robot).to be_an_instance_of(Robot)
+      expect(robot.x).to eq(2)
+      expect(robot.y).to eq(3)
+      expect(robot.face).to eq(Direction::E)
+    end
+    it "when x and y is not numeric" do
+      expect{Robot.new("",0,Direction::N)}.to raise_error(ArgumentError)
+      expect{Robot.new(0,"",Direction::N)}.to raise_error(ArgumentError)
+      expect{Robot.new("","",Direction::N)}.to raise_error(ArgumentError)
     end
   end
 
