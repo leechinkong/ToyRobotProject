dd7481f1c1b6e4c110cb9bb164b42729135253e6
LeeChinKong <lee.c.kong@gmail.com>
2014-11-10 13:53:56 +1100

    Update Table implementation
    * added validate(x,y) method
    * refactor Table test

--- lib/toy_robot/model/table.rb
+++ lib/toy_robot/model/table.rb
@@ -1,4 +1,29 @@
 # Table class
 
 class Table
+
+  # The width of the table
+  attr_reader:width
+  # The height of the table
+  attr_reader:height
+
+  # Initialize the table with width and height (in units).
+  # = Parameters
+  # - +width+:: the width of the table
+  # - +height+:: the height of the table
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+
+  # Validate x and y position to make sure the robot does not fall off the table.
+  # = Parameters
+  # - +x+:: the x position of the robot
+  # - +y+:: the y position of the robot
+  # = Returns
+  # - true when x and y is valid
+  def validate(x, y)
+    return x >= 0 && x < @width && y >= 0 && y < @height
+  end
+
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -6,46 +6,50 @@ describe Table do
   # Test initialize
   describe "#new" do
     it "is an instance of Table" do
-      @table = Table.new(5,5)
-      expect(@table).to be_an_instance_of(Table)
-      expect(@table.width).to eq(5)
-      expect(@table.height).to eq(5)
+      table = Table.new(5,5)
+      expect(table).to be_an_instance_of(Table)
+      expect(table.width).to eq(5)
+      expect(table.height).to eq(5)
 
-      @table = Table.new(10,8)
-      expect(@table).to be_an_instance_of(Table)
-      expect(@table.width).to eq(10)
-      expect(@table.height).to eq(8)
+      table = Table.new(10,8)
+      expect(table).to be_an_instance_of(Table)
+      expect(table.width).to eq(10)
+      expect(table.height).to eq(8)
     end
   end
 
   
    # Test validateXY
   describe "#validate" do
-    it "x is invalid" do
-      expect(@controller.validate(-1,0)).to eq(false)
-      expect(@controller.validate(5,0)).to eq(false)
-      expect(@controller.validate(10,0)).to eq(false)
+    it "when x is invalid" do
+      table = Table.new(5,5)
+      expect(table.validate(-1,0)).to eq(false)
+      expect(table.validate(5,0)).to eq(false)
+      expect(table.validate(10,0)).to eq(false)
     end
 
-    it "y is invalid" do
-      expect(@controller.validate(0,-1)).to eq(false)
-      expect(@controller.validate(0,5)).to eq(false)
-      expect(@controller.validate(0,10)).to eq(false)
+    it "when y is invalid" do
+      table = Table.new(5,5)
+      expect(table.validate(0,-1)).to eq(false)
+      expect(table.validate(0,5)).to eq(false)
+      expect(table.validate(0,10)).to eq(false)
     end
 
-    it "x and y is invalid" do
-      expect(@controller.validate(-1,-1)).to eq(false)
-      expect(@controller.validate(5,5)).to eq(false)
-      expect(@controller.validate(10,10)).to eq(false)
+    it "when x and y is invalid" do
+      table = Table.new(5,5)
+      expect(table.validate(-1,-1)).to eq(false)
+      expect(table.validate(5,5)).to eq(false)
+      expect(table.validate(10,10)).to eq(false)
     end
     
-    it "x and y is valid" do
-      expect(@controller.validate(0,0)).to eq(true)
-      expect(@controller.validate(0,4)).to eq(true)
-      expect(@controller.validate(1,2)).to eq(true)
-      expect(@controller.validate(3,2)).to eq(true)
-      expect(@controller.validate(4,0)).to eq(true)
-      expect(@controller.validate(4,4)).to eq(true)
+    it "when x and y is valid" do
+      table = Table.new(5,5)
+      expect(table.validate(0,0)).to eq(true)
+      expect(table.validate(0,4)).to eq(true)
+      expect(table.validate(1,2)).to eq(true)
+      expect(table.validate(3,2)).to eq(true)
+      expect(table.validate(4,0)).to eq(true)
+      expect(table.validate(4,4)).to eq(true)
     end
   end
 end
