fcfb6e41ce1455e9ae75d37f5cc01713521b8030
LeeChinKong <lee.c.kong@gmail.com>
2014-11-13 22:38:12 +1100

    Added robot tests to use direction classes
    * added turnLeft, turnRight, move tests in preparation for switching
    from constants to use direction classes

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -10,13 +10,13 @@ describe Robot do
       expect(robot).to be_an_instance_of(Robot)
       expect(robot.x).to eq(0)
       expect(robot.y).to eq(0)
-      expect(robot.face).to eq(Direction::N)
+      expect(robot.face).to be_an_instance_of(Direction::North)
 
       robot = Robot.new(2,3,Direction::E)
       expect(robot).to be_an_instance_of(Robot)
       expect(robot.x).to eq(2)
       expect(robot.y).to eq(3)
-      expect(robot.face).to eq(Direction::E)
+      expect(robot.face).to be_an_instance_of(Direction::East)
     end
     it "when x and y is not numeric" do
       expect{Robot.new("",0,Direction::N)}.to raise_error(ArgumentError)
@@ -39,7 +39,7 @@ describe Robot do
       expect{robot.update(1,1,Direction::E)}.not_to raise_error
       expect(robot.x).to eq(1)
       expect(robot.y).to eq(1)
-      expect(robot.face).to eq(Direction::E)
+      expect(robot.face).to be_an_instance_of(Direction::East)
     end
   end
 
@@ -68,7 +68,67 @@ describe Robot do
     it "when face is valid" do
       robot = Robot.new(0,0,Direction::N)
       expect{robot.updateFace(Direction::E)}.not_to raise_error
-      expect(robot.face).to eq(Direction::E)
+      expect(robot.face).to be_an_instance_of(Direction::East)
+    end
+  end
+
+  # Test turnLeft
+  describe "#turnLeft" do
+    it "when face is NORTH" do
+      robot = Robot.new(0,0,Direction::N)
+      expect(robot.turnLeft).to be_an_instance_of(Direction::West)
+    end
+    it "when face is EAST" do
+      robot = Robot.new(0,0,Direction::E)
+      expect(robot.turnLeft).to be_an_instance_of(Direction::North)
+    end
+    it "when face is SOUTH" do
+      robot = Robot.new(0,0,Direction::S)
+      expect(robot.turnLeft).to be_an_instance_of(Direction::East)
+    end
+    it "when face is WEST" do
+      robot = Robot.new(0,0,Direction::W)
+      expect(robot.turnLeft).to be_an_instance_of(Direction::South)
+    end
+  end
+
+  # Test turnRight
+  describe "#turnRight" do
+    it "when face is NORTH" do
+      robot = Robot.new(0,0,Direction::N)
+      expect(robot.turnRight).to be_an_instance_of(Direction::East)
+    end
+    it "when face is EAST" do
+      robot = Robot.new(0,0,Direction::E)
+      expect(robot.turnRight).to be_an_instance_of(Direction::South)
+    end
+    it "when face is SOUTH" do
+      robot = Robot.new(0,0,Direction::S)
+      expect(robot.turnRight).to be_an_instance_of(Direction::West)
+    end
+    it "when face is WEST" do
+      robot = Robot.new(0,0,Direction::W)
+      expect(robot.turnRight).to be_an_instance_of(Direction::North)
+    end
+  end
+
+  # Test move
+  describe "#move" do
+    it "when face is NORTH" do
+      robot = Robot.new(0,0,Direction::N)
+      expect(robot.move).to eq([0,1])
+    end
+    it "when face is EAST" do
+      robot = Robot.new(0,0,Direction::E)
+      expect(robot.move).to eq([1,0])
+    end
+    it "when face is SOUTH" do
+      robot = Robot.new(0,0,Direction::S)
+      expect(robot.move).to eq([0,-1])
+    end
+    it "when face is WEST" do
+      robot = Robot.new(0,0,Direction::W)
+      expect(robot.move).to eq([-1,0])
     end
   end
 
